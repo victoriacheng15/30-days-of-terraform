c655d6fa9cba94398be33eba7fffa049387ce580
Victoria Cheng <victoria.cheng15@gmail.com>
2026-04-21 20:50:22 +0000

    feat(day-27): add repo-wide CI fmt and validate workflow

---
+++ day-27/main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  tags = merge(
+    {
+      environment = var.environment
+      project     = "30-days-of-opentofu"
+      day         = "27"
+      managed_by  = "github-actions"
+    },
+    var.common_tags
+  )
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = local.tags
+}
+
+resource "random_string" "storage_suffix" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
+resource "azurerm_storage_account" "state" {
+  name                            = lower("${var.storage_account_name_prefix}${random_string.storage_suffix.result}")
+  resource_group_name             = azurerm_resource_group.main.name
+  location                        = azurerm_resource_group.main.location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  account_kind                    = "StorageV2"
+  min_tls_version                 = "TLS1_2"
+  https_traffic_only_enabled      = true
+  allow_nested_items_to_be_public = false
+  tags                            = local.tags
+}
